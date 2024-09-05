6670a3c870b5c175f2272c9527d0726212c08eec
dariojoel0905 <dariojoel0905@gmail.com>
2024-09-05 14:06:21 +0200

    Created the project again

---
+++ Bookonomie/Bookonomie/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bookonomie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
