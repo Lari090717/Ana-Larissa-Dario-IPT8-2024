9d2652d0b18078c6b77df090e91acc394846da74
Lari07 <Larissa_Fahrni@sluz.ch>
2024-09-12 15:46:25 +0200

    made the structure for the login and register

---
+++ Bookonomie/Bookonomie/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using Bookonomie.Models;
+using Bookonomie.Views.Login;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Bookonomie.Controllers
+{
+    public class LoginController: Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
